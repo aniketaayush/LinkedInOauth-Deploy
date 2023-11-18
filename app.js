3a0ec67aef2d66de5a48ca9e5ef49797e8da5351
aniketaayush <aniketaayush5@gmail.com>
2023-11-19 04:18:38 +0530

    Added Code to serve react pages through express

--- app.js
+++ app.js
@@ -221,6 +221,14 @@ app.post("/saveResumeData", async(req, res) => {
   res.send({status: "success"});
 })
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('frontend/build'));
+  const path = require('path');
+  app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
+
 app.listen(process.env.PORT, () => {
   console.log("Server Started");
 });
